6c14a425c21d5fc9ce185bec5f9d9650c7ffa606
Keith <kg.anderson@outlook.com>
2021-02-26 11:10:39 +1300

    Install firebase .env and create firebase app

--- client/index.jsx
+++ client/index.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { render } from 'react-dom'
 import { HashRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-
 import App from './App'
 import store from './store'
+require('dotenv').config()
 
 if (module.hot) {
   module.hot.accept()
