81375ef2b29f5256450bd37623e12d1af9e3d093
sirchras <cjlynch@hotmail.co.nz>
2021-02-25 14:39:10 +1300

    add image input
    it's just a f'n input field

---
+++ client/features/camera/Camera.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Camera () {
+  return (
+    <>
+      <h2>hello world!</h2>
+      <input type="file" id="imageFile" capture="user" accept="image/*"/>
+    </>
+  )
+}
+
+export default Camera
