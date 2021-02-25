9c45700c1d673bcdfd2ba5c96537be25b157b522
Mariano Zeppa <zeppamariano@gmail.com>
2021-02-26 11:06:48 +1300

    update on Icon

--- client/features/map/Markers.jsx
+++ client/features/map/Markers.jsx
@@ -5,6 +5,6 @@ import { IconLocation } from './IconLocation'
 
 export default function Markers () {
   return (
-    <Marker position={{ lat: '-36.8750570540076', lng: '174.76811551843952', icon:{IconLocation} }} />
+    <Marker position={{ lat: '-36.8750570540076', lng: '174.76811551843952', icon: { IconLocation } }} />
   )
 }
