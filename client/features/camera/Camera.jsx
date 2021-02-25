3f75b039dda05a0716ffff04c9a4b56df6a6ce70
sirchras <cjlynch@hotmail.co.nz>
2021-02-25 15:16:20 +1300

    add handler for img input

--- client/features/camera/Camera.jsx
+++ client/features/camera/Camera.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Camera () {
+  const [img, setImg] = useState(null)
+
+  function onChange (e) {
+    setImg(e.target.files[0])
+  }
   return (
     <>
       <h2>hello world!</h2>
-      <input type="file" id="imageFile" capture="user" accept="image/*"/>
+      <input type="file" id="imageFile" capture="environment" accept="image/*" onChange={onChange}/>
+      {
+        img
+          ? <img src={URL.createObjectURL(img)}/>
+          : <p>no image specified</p>
+      }
     </>
   )
 }
