1f9190d4d8054e186dded528891a4341ee789409
Mariano Zeppa <zeppamariano@gmail.com>
2021-02-27 12:23:44 +1300

    login form base style

--- client/features/Auth/Login.jsx
+++ client/features/Auth/Login.jsx
@@ -29,7 +29,8 @@ function Login ({ history }) {
 
   return (
     <div>
-      <h1>Log in</h1>
+
+      {/* <h1>Log in</h1>
       <form onSubmit={handleLogin}>
         <label>
           Email
@@ -41,7 +42,21 @@ function Login ({ history }) {
         </label>
         <button type="submit">Log in</button>
       </form>
-      <Link to="/register"><p>No Account? Register here</p></Link>
+      <Link to="/register"><p>No Account? Register here</p></Link> */}
+      <div className="shadow-2xl">
+        <div className="min-h-screen px-10 py-32 text-xl bg-purple-600 ">
+          <form className="mx-auto md:w-1/2 "action="">
+            <div className="flex items-center bg-purple-300 border-b border-purple-500 rounded-t-lg">
+              <label htmlFor="name" className="w-20 mr-8 text-right text-purple-100">Name</label>
+              <input type="text" id="name" name="name" placeholder="Insert your name" className="flex-1 p-4 pl-0 text-white placeholder-purple-200 bg-transparent outline-none"/>
+            </div >
+            <div className="flex items-center mb-10 bg-purple-300 rounded-b-lg"><label htmlFor="twitter" className="w-20 mr-8 text-right  text-purple-100">Twitter</label>
+              <input type="text" id="twitter" name="twitter" placeholder="Insert your twitter" className="flex-1 p-4 pl-0 text-white placeholder-purple-200 bg-transparent outline-none"/></div>
+            <button className="block w-full py-3 font-bold text-white bg-pink-400 rounded shadow-2xl">Submit</button>
+          </form>
+        </div>
+      </div>
+
     </div>
   )
 }
