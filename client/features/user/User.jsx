b50962b750f32247d34cda0da21b391002daa51e
Keith <kg.anderson@outlook.com>
2021-02-27 11:51:53 +1300

    Show user email in Users component

--- client/features/user/User.jsx
+++ client/features/user/User.jsx
@@ -1,20 +1,36 @@
-import React, { useContext } from 'react'
-import { withRouter, Redirect } from 'react-router'
+import React, { useContext, useEffect, useState } from 'react'
+import { Redirect } from 'react-router'
 import Logout from '../auth/Logout'
 import { AuthContext } from '../auth/GetAuthState'
+import app from '../../firebase'
 
 function User () {
-  const { currentUser } = useContext(AuthContext)
+  useEffect(() => {
+    fetchUserData()
+  }, [])
 
+  const [userDetails, setUserDetails] = useState({})
+
+  const { currentUser } = useContext(AuthContext)
   if (!currentUser) {
     return <Redirect to="/login" />
   }
+
+  const fetchUserData = async () => {
+    const userRef = app.firestore().collection('users').where('id', '==', `${currentUser.uid}`)
+    const user = await userRef.get()
+    user.docs.forEach(item => {
+      setUserDetails(item.data())
+    })
+  }
+
   return (
     <>
+      <h3>Email:</h3>{userDetails.email}
       <p>this is the users page</p>
       <Logout/>
     </>
   )
 }
 
-export default withRouter(User)
+export default User
