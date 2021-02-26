56d1748a0031d85e4cc6d0f682f927a76d83a3c1
Kenneth Anderson <ken@kenneth.net.nz>
2021-02-26 13:55:38 +1300

    padding visual change to footer

--- client/features/nav/Footer.jsx
+++ client/features/nav/Footer.jsx
@@ -4,7 +4,7 @@ import NavButton from './NavButton'
 export default function Footer () {
   return (
     <footer className="text-6xl shadow-lg transform rotate-180">
-      <div className="flex justify-between p-8 px-12 transform rotate-180">
+      <div className="flex justify-between p-4 px-12 transform rotate-180">
         {/* map camera feed */}
         <NavButton target="/map" className="fas fa-map text-gradient bg-gradient-to-r from-pink-400 to-indigo-500" />
         <NavButton target="/camera" className="fas fa-camera text-gradient bg-gradient-to-r from-pink-400 to-indigo-500" />
