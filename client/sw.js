83271fbdfecdf863ce08bd472bd9c539c6e59319
Kenneth Anderson <ken@kenneth.net.nz>
2021-02-27 14:17:31 +1300

    service worker?

--- client/sw.js
+++ client/sw.js
@@ -1,1 +1,15 @@
-console.log('hello from sw.js')
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('sw-cache').then(function (cache) {
+      return cache.add('index.html')
+    })
+  )
+})
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request)
+    })
+  )
+})
