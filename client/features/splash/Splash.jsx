6fae43ac55a7195203de2ab1dcbf092c3f4f3f8e
Kenneth Anderson <ken@kenneth.net.nz>
2021-02-27 15:45:09 +1300

    implement navigation from splash

--- client/features/splash/Splash.jsx
+++ client/features/splash/Splash.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import { useHistory } from 'react-router'
 
 export default function Splash () {
+  const history = useHistory()
+
+  function navTo (e, route) {
+    e.preventDefault()
+    history.push(route)
+  }
+
   return (
     <div className="flex flex-col items-center justify-center h-full bg-gray-50">
       <div className="text-gradient bg-gradient-to-r from-pink-400 to-indigo-500">
         <i className="fas fa-cat text-9xl"></i>
       </div>
       <div className="flex justify-around w-full px-6 pt-20 text-gray-50 placeholder-opacity-20">
-        <div className="w-32 p-4 text-xl font-bold text-center rounded-xl bg-gradient-to-r from-pink-400 to-indigo-500">Login</div>
-        <div className="w-32 p-4 text-xl font-bold text-center rounded-xl bg-gradient-to-r from-pink-400 to-indigo-500">Register</div>
+        <div onClick={e => navTo(e, '/login')} className="w-32 p-4 text-xl font-bold text-center rounded-xl bg-gradient-to-r from-pink-400 to-indigo-500">Login</div>
+        <div onClick={e => navTo(e, '/register')} className="w-32 p-4 text-xl font-bold text-center rounded-xl bg-gradient-to-r from-pink-400 to-indigo-500">Register</div>
       </div>
     </div>
   )
