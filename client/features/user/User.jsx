9e9906ae4f91412f6ae83f9e11548f159ff63ada
Keith <kg.anderson@outlook.com>
2021-02-27 13:23:42 +1300

    Retrieve users sightings from firestore

--- client/features/user/User.jsx
+++ client/features/user/User.jsx
@@ -9,7 +9,12 @@ function User () {
     fetchUserData()
   }, [])
 
+  useEffect(() => {
+    fetchUserSightings()
+  }, [])
+
   const [userDetails, setUserDetails] = useState({})
+  const [userSightings, setUserSightings] = useState([])
 
   const { currentUser } = useContext(AuthContext)
   if (!currentUser) {
@@ -24,10 +29,27 @@ function User () {
     })
   }
 
+  const fetchUserSightings = async () => {
+    const list = []
+    const sightingsRef = app.firestore().collection('sightings').where('userID', '==', `${currentUser.uid}`)
+    const sighting = await sightingsRef.get()
+    sighting.docs.forEach(item => {
+      list.push(item.data())
+    })
+    setUserSightings(list)
+  }
+
+  console.log(userSightings)
+
   return (
     <>
       <h3>Email:</h3>{userDetails.email}
       <p>this is the users page</p>
+      {userSightings.map((result, index) => (
+        <div key={index}>
+          <img src={result.photoUrl} alt="catpic"/>
+        </div>
+      ))}
       <Logout/>
     </>
   )
