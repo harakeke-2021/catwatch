055e867599e48855238bd93296537e0c787cef84
Mariano Zeppa <zeppamariano@gmail.com>
2021-02-27 14:17:38 +1300

    loginFormOK

--- client/features/Auth/Login.jsx
+++ client/features/Auth/Login.jsx
@@ -31,13 +31,13 @@ function Login ({ history }) {
     <div>
 
       <div className="shadow-2xl">
-        <div className="min-h-screen px-10 py-32 text-xl bg-indigo-500 ">
+        <div className="h-full w-full max-h-full px-10 py-32 text-xl bg-indigo-500 ">
           <form className="mx-auto md:w-1/2" onSubmit={handleLogin}>
             <div className="flex items-center  bg-indigo-300 border-b border-indigo-500 rounded-t-lg">
               <label htmlFor="name" className="w-20 mr-8 text-right text-pink-100">Email</label>
-              <input name="email" type="email" placeholder="Email" autoComplete="on" className="flex-1 p-4 pl-0 text-white ml-5 placeholder-purple-200 bg-transparent outline-none"/>
+              <input name="email" type="email" placeholder="Email" autoComplete="on" className="flex-1 p-4 pl-0 text-white placeholder-purple-200 bg-transparent outline-none"/>
             </div >
-            <div className="flex items-center mb-10  bg-indigo-300 rounded-b-lg"><label htmlFor="twitter" className="w-20 mr-8 text-right ml-5  text-pink-100">Password</label>
+            <div className="flex items-center mb-10  bg-indigo-300 rounded-b-lg"><label htmlFor="twitter" className="w-20 mr-8 text-right ml-5 text-pink-100">Password</label>
               <input name="password" type="password" placeholder="Password" autoComplete="on" className="flex-1 p-4 pl-0 text-white placeholder-purple-200 bg-transparent outline-none"/></div>
             <button type="submit" className="block w-full py-3 font-bold text-white bg-pink-400 rounded shadow-2xl">Log In</button>
             <p className="flex-1 p-4 pl-0 text-pink-100">No account? Register <Link to="/register" className='text-pink-400 underline'>here</Link></p>
