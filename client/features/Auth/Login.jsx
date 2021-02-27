0c4436194316bda0ae1f0d3250c01f50046e446f
Mariano Zeppa <zeppamariano@gmail.com>
2021-02-27 13:09:22 +1300

    loginformalmostdone

--- client/features/Auth/Login.jsx
+++ client/features/Auth/Login.jsx
@@ -30,35 +30,25 @@ function Login ({ history }) {
   return (
     <div>
 
-      {/* <h1>Log in</h1>
-      <form onSubmit={handleLogin}>
-        <label>
-          Email
-          <input name="email" type="email" placeholder="Email" autoComplete="on" />
-        </label>
-        <label>
-          Password
-          <input name="password" type="password" placeholder="Password" autoComplete="on"/>
-        </label>
-        <button type="submit">Log in</button>
-      </form>
-      <Link to="/register"><p>No Account? Register here</p></Link> */}
       <div className="shadow-2xl">
-        <div className="min-h-screen px-10 py-32 text-xl bg-purple-600 ">
-          <form className="mx-auto md:w-1/2 "action="">
-            <div className="flex items-center bg-purple-300 border-b border-purple-500 rounded-t-lg">
-              <label htmlFor="name" className="w-20 mr-8 text-right text-purple-100">Name</label>
-              <input type="text" id="name" name="name" placeholder="Insert your name" className="flex-1 p-4 pl-0 text-white placeholder-purple-200 bg-transparent outline-none"/>
+        <div className="min-h-screen px-10 py-32 text-xl bg-indigo-500 ">
+          <form className="mx-auto md:w-1/2" onSubmit={handleLogin}>
+            <div className="flex items-center  bg-indigo-300 border-b border-indigo-500 rounded-t-lg">
+              <label htmlFor="name" className="w-20 mr-8 text-right text-pink-100">Email</label>
+              <input name="email" type="email" placeholder="Email" autoComplete="on" className="flex-1 p-4 pl-0 text-white ml-5 placeholder-purple-200 bg-transparent outline-none"/>
             </div >
-            <div className="flex items-center mb-10 bg-purple-300 rounded-b-lg"><label htmlFor="twitter" className="w-20 mr-8 text-right  text-purple-100">Twitter</label>
-              <input type="text" id="twitter" name="twitter" placeholder="Insert your twitter" className="flex-1 p-4 pl-0 text-white placeholder-purple-200 bg-transparent outline-none"/></div>
-            <button className="block w-full py-3 font-bold text-white bg-pink-400 rounded shadow-2xl">Submit</button>
+            <div className="flex items-center mb-10  bg-indigo-300 rounded-b-lg"><label htmlFor="twitter" className="w-20 mr-8 text-right ml-5  text-pink-100">Password</label>
+              <input name="password" type="password" placeholder="Password" autoComplete="on" className="flex-1 p-4 pl-0 text-white placeholder-purple-200 bg-transparent outline-none"/></div>
+            <button type="submit" className="block w-full py-3 font-bold text-white bg-pink-400 rounded shadow-2xl">Log In</button>
+            <p className="flex-1 p-4 pl-0 text-pink-100">No account? Register <Link to="/register" className='text-pink-400 underline'>here</Link></p>
           </form>
         </div>
       </div>
 
     </div>
+
   )
 }
+// text-gradient bg-gradient-to-r from-pink-400 to-indigo-500
 
 export default Login
