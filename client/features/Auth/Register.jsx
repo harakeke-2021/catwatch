6aca2dd36e116b458c4f46a0d0b12e319de54742
Mariano Zeppa <zeppamariano@gmail.com>
2021-02-27 14:57:41 +1300

    login&registerFormOK

--- client/features/Auth/Register.jsx
+++ client/features/Auth/Register.jsx
@@ -31,23 +31,24 @@ function Register ({ history }) {
   }
 
   return (
-    <section>
-      <div>
-        <h1>Sign up</h1>
-        <form onSubmit={handleRegister}>
-          <label>Email
-            <input name="email" type="email" placeholder="Email" autoComplete="on" />
-          </label>
 
-          <label>Password
-            <input name="password" type="password" placeholder="Password" autoComplete="on" />
-          </label>
-
-          <button type="submit">Sign Up</button>
-        </form>
-        <Link to="/login"><p>Already part of CATWATCH? Login here</p></Link>
+    <div>
+      <div className="shadow-2xl">
+        <div className="w-full h-full max-h-full px-10 py-32 text-xl bg-indigo-500">
+          <form className="mx-auto" onSubmit={handleRegister}>
+            <div className="flex items-center  bg-indigo-300 border-b border-indigo-500 rounded-t-lg">
+              <label htmlFor="name" className="w-20 mr-8 text-right text-pink-100">Email</label>
+              <input name="email" type="email" placeholder="Email" autoComplete="on" className="flex-1 p-4 pl-0 text-white placeholder-purple-200 bg-transparent outline-none"/>
+            </div >
+            <div className="flex items-center mb-10  bg-indigo-300 rounded-b-lg"><label htmlFor="twitter" className="w-20 ml-5 mr-8 text-right text-pink-100">Password</label>
+              <input name="password" type="password" placeholder="Password" autoComplete="on" className="flex-1 p-4 pl-0 text-white placeholder-purple-200 bg-transparent outline-none"/></div>
+            <button type="submit" className="block w-full py-3 font-bold text-white bg-pink-400 rounded shadow-2xl">Register</button>
+            <p className="flex-1 p-4 pl-0 text-pink-100 text-xs">Already part of CATWATCH? Log in <Link to="/login" className='text-pink-400 underline'>here</Link></p>
+          </form>
+        </div>
       </div>
-    </section>
+
+    </div>
   )
 }
 
