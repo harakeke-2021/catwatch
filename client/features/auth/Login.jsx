c1ff06c953f3e8a11731bb2cafbc8cd1e7c70e53
sirchras <cjlynch@hotmail.co.nz>
2021-03-01 11:22:56 +1300

    update form labels and input ids

--- client/features/auth/Login.jsx
+++ client/features/auth/Login.jsx
@@ -33,11 +33,12 @@ function Login ({ history }) {
         <div className="flex flex-col w-screen h-full px-10 py-32 text-xl bg-indigo-500">
           <form className="items-center w-full mx-auto my-auto" onSubmit={handleLogin}>
             <div className="flex items-center bg-indigo-300 border-b border-indigo-500 rounded-t-lg">
-              <label htmlFor="name" className="w-20 ml-2 mr-8 text-pink-100">Email</label>
-              <input name="email" type="email" placeholder="Email" autoComplete="on" className="flex-1 p-4 pl-0 text-white placeholder-purple-200 bg-transparent outline-none"/>
+              <label htmlFor="email" className="w-20 ml-2 mr-8 text-pink-100">Email</label>
+              <input id="email" name="email" type="email" placeholder="Email" autoComplete="on" className="flex-1 p-4 pl-0 text-white placeholder-purple-200 bg-transparent outline-none"/>
             </div >
-            <div className="flex items-center mb-10  bg-indigo-300 rounded-b-lg"><label htmlFor="twitter" className="w-20 ml-2 mr-8 text-pink-100">Password</label>
-              <input name="password" type="password" placeholder="Password" autoComplete="on" className="flex-1 p-4 pl-0 text-white placeholder-purple-200 bg-transparent outline-none"/></div>
+            <div className="flex items-center mb-10  bg-indigo-300 rounded-b-lg">
+              <label htmlFor="password" className="w-20 ml-2 mr-8 text-pink-100">Password</label>
+              <input id="password" name="password" type="password" placeholder="Password" autoComplete="on" className="flex-1 p-4 pl-0 text-white placeholder-purple-200 bg-transparent outline-none"/></div>
             <button type="submit" className="block w-full py-3 font-bold text-white bg-pink-400 rounded shadow-2xl">Log In</button>
             <p className="flex-1 p-4 pl-0 text-xs text-pink-100">No account? Register <Link to="/register" className='text-pink-400 underline'>here</Link></p>
           </form>
