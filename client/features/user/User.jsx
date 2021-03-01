c05a2eb394c69390ba9fbe6667373a3199ba9d66
Keith <kg.anderson@outlook.com>
2021-03-02 08:15:58 +1300

    Add in username to user page

--- client/features/user/User.jsx
+++ client/features/user/User.jsx
@@ -46,7 +46,8 @@ function User () {
           <div className="flex flex-col items-center justify-center h-screen">
             <img src={userDetails.userPicture}
               alt="" className="w-32 h-32 mt-5 border-2 border-gray-400 rounded-full"/>
-            <h1 className="pt-5 text-xl font-semibold text-gradient bg-gradient-to-r from-indigo-500 to-pink-400 rounded-md">{userDetails.email}</h1>
+            <h1 className="pt-5 text-xl font-semibold text-gradient bg-gradient-to-r from-indigo-500 to-pink-400 rounded-md">{userDetails.username}</h1>
+            <h1 className="pt-5 text-lg font-semibold text-gradient bg-gradient-to-r from-indigo-500 to-pink-400 rounded-md">{userDetails.email}</h1>
             <h1 className="p-4 text-sm text-gradient bg-gradient-to-r from-indigo-500 to-pink-300">Auckland, New Zealand</h1>
             <div className="w-11/12 h-full mt-2 overflow-y-hidden bg-gradient-to-bl from-indigo-200 to-pink-300 rounded-md">
               <div className="flex flex-wrap w-full m-0 justify-left h-max">
