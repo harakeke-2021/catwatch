fe4243c7b4c35e93076091c1e50b9ace6a651d21
sirchras <cjlynch@hotmail.co.nz>
2021-03-03 10:39:44 +1300

    separate heatmap code into own useeffect

--- client/features/map/Map.jsx
+++ client/features/map/Map.jsx
@@ -9,29 +9,32 @@ import { updateLocation } from './geolocHelper'
 
 export default function Map () {
   const posts = useSelector(selectPosts)
+  const map = L.map('map', { doubleClickZoom: false })
 
   const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(fetchPosts())
-  }, [!posts])
+  }, [])
 
   useEffect(() => {
-    const map = L.map('map', { doubleClickZoom: false })
-    const points = posts.filter(post => !!post.location)
-      .map(post => {
-        const { latitude, longitude } = post.location
-        return [latitude, longitude]
-      })
-
     map.locate({ setView: true, maxZoom: 16 })
     map.once('locationfound', (evt) => updateLocation(dispatch, evt))
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map)
-    L.heatLayer(points).addTo(map)
   }, [])
+
+  useEffect(() => {
+    const points = posts.filter(post => !!post.location)
+      .map(post => {
+        const { latitude, longitude } = post.location
+        return [latitude, longitude]
+      })
+
+    L.heatLayer(points).addTo(map)
+  }, [posts])
 
   return (
     <div id="map" style={{ height: '100vh' }}></div>
